0eb69d01b33735f51905cdf6603cf20b8306c4a0
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2021-01-09 13:12:46 +0530

    Added await/async

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+
 const amqp = require("amqplib");
 const url = process.env.AMQP_URL;
+const queue = "buzzle-jobs";
 
 async function main() {
   const connection = await amqp.connect(url);
   const channel = await connection.createChannel();
 
-  const queue = "buzzle-jobs";
+  // jobs queue
   await channel.assertQueue(queue, { durable: false });
   channel.consume(
     queue,
@@ -27,6 +30,23 @@ async function main() {
       noAck: true,
     }
   );
+
+  // creators queue
+  await channel.assertQueue(queue, { durable: false });
+  channel.consume("buzzle-creators", ({ content }) => {
+    const { data, eventType } = JSON.parse(content.toString());
+
+    switch (eventType) {
+      case "created":
+        console.log("Send creator created mail");
+        break;
+      case "updated":
+        console.log("Send creator updated mail");
+        break;
+      case "deleted":
+        console.log("Send creator deleted mail");
+    }
+  });
 }
 
 main().catch(console.log);
